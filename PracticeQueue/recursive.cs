a4e2e2f88967f650015a07138c1cb53b951db932
Joony15 <noddykk15@gmail.com>
2016-07-18 15:04:40 +0900

    practice recursive

---
+++ PracticeQueue/recursive.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQueue
+{
+    class Program
+    {
+        int partition(int n ,int m)
+        {
+            int count = 0, i;
+
+            if (n < m)
+            {
+                m = n;
+            }
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            for (i = 1; i <= m; i++)
+            {
+                count = count + partition(n - i, i);
+            }
+            return count;
+        }
+        static void Main()
+        {
+            int n, m;
+
+            Program mProgram = new Program();
+
+            Console.WriteLine("please input number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n);
+            Console.WriteLine("please input which");
+
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(m);
+            Console.WriteLine(mProgram.partition(n,m));
+
+            
+        }
+    }
+  
+}
